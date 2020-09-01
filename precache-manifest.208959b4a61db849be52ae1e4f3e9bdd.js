1ae5911f50172e7a31fd4663e13e05358bb0bf12
Deyvi De La Cruz <dekhan06@gmail.com>
2020-08-31 22:51:33 -0500

    deploy

---
+++ precache-manifest.208959b4a61db849be52ae1e4f3e9bdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12a952de9824042fc50",
+    "url": "/vue-app-news/css/app.efa95f47.css"
+  },
+  {
+    "revision": "4f3e0b95712721f42b6c",
+    "url": "/vue-app-news/css/chunk-vendors.97989550.css"
+  },
+  {
+    "revision": "66c3f8facda619892932969f7d41b75b",
+    "url": "/vue-app-news/index.html"
+  },
+  {
+    "revision": "90a22cc122fbbc78d91b",
+    "url": "/vue-app-news/js/about.7454158f.js"
+  },
+  {
+    "revision": "d12a952de9824042fc50",
+    "url": "/vue-app-news/js/app.c2d9c499.js"
+  },
+  {
+    "revision": "4f3e0b95712721f42b6c",
+    "url": "/vue-app-news/js/chunk-vendors.9a68ae0c.js"
+  },
+  {
+    "revision": "f7f80096b17d9529e833fd09dd35607f",
+    "url": "/vue-app-news/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-news/robots.txt"
+  }
+]);
